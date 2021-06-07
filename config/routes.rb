8d75daee1fcb22c4dd33d2cda2c3efa5bb5b41b4
ricardodepaula2021 <ricardo.depaula@yahoo.com.br>
2021-06-07 10:09:34 -0300

    pages controller included via 'rails g controller pages'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  def home
-  end
-
-  def about
-  end
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 end
